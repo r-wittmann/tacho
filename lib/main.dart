39485f9956f87cf33340dc04da76f4cee979673a
Rainer Wittmann <rainer.wittmann@comsysto.com>
2018-10-21 17:22:25 +0200

    fix phone orientation to portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tacho/view/ContainerView.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(new MyApp());
+  });
+}
 
 class MyApp extends StatelessWidget {
   @override
